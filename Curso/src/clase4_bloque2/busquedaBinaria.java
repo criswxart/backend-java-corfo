f5f8d0137f34bc5a31b1195f71187b21e5d91cac
17999950 <17999950@Sruiz.COVEPA.LOCAL>
2024-10-24 12:24:59 -0300

    Agrego busqueda binaria

---
+++ Curso/src/clase4_bloque2/busquedaBinaria.java
@@ -0,0 +1,51 @@
+package clase4_bloque2;
+
+import java.util.ArrayList;
+
+public class busquedaBinaria {
+
+	public static void main(String[] args) {
+		ArrayList<Integer>numerosOrdenados=new ArrayList<>();
+		
+		for(int i=0; i<=10;i+=2) {
+			numerosOrdenados.add(i);
+			//System.out.println(numerosOrdenados.getLast());
+		}
+		int repuesta=buscarValor(numerosOrdenados, 30);
+		if(repuesta==-1) {
+			System.out.println("Valor no esta en el array");
+		}else {
+			System.out.println("Valor se encuentra en posicion "+repuesta);
+		}
+		
+		
+		// TODO Auto-generated method stub
+
+	}
+	
+	public static int buscarValor(ArrayList<Integer>array, int valorDeseado) {
+		int indiceValorDeseado=-1;
+		int limiteInferior=0;
+		int limiteSuperior=array.size()-1;
+		int indiceAMirar;
+		
+		do {
+			indiceAMirar=(limiteInferior+limiteSuperior)/2;
+			System.out.println("Inferior "+limiteInferior +" / Superior "+limiteSuperior+" / Indice a mirar "+indiceAMirar);		
+			if(array.get(indiceAMirar)==valorDeseado) {
+				indiceValorDeseado=indiceAMirar;	
+				break;
+			}else if(array.get(indiceAMirar)>valorDeseado) {				
+				limiteSuperior=indiceAMirar-1;
+			}else {
+				limiteInferior=indiceAMirar+1;					
+			}	
+		}while((array.get(indiceAMirar)!=valorDeseado)&&(limiteInferior<limiteSuperior));
+		
+		
+		return indiceValorDeseado;			
+		
+		
+	}
+
+}
